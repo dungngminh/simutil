ac1a9bb4f4747328fb29b1836b744b8945473d2b
dungngminh <63831488+dungngminh@users.noreply.github.com>
2026-02-16 15:22:09 +0700

    init simutil

---
+++ bin/simutil.dart
@@ -0,0 +1,5 @@
+import 'package:simutil/simutil.dart' as simutil;
+
+void main(List<String> arguments) {
+  print('Hello world: ${simutil.calculate()}!');
+}
---
+++ test/simutil_test.dart
@@ -0,0 +1,8 @@
+import 'package:simutil/simutil.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
